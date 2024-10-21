fe8eaabc084e4e270d9212e0e159ca9c123186cf
infinity <1833177514@qq.com>
2024-10-21 18:49:33 +0800

    feat(database): Create model OrganizerActivity and Alter field school on volunteer

--- volunteer_management/api/admin.py
+++ volunteer_management/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Activity, ActivityStatus, Organizer, Volunteer, VolunteerActivity, ActivityApplication
+from .models import Activity, ActivityStatus, Organizer, Volunteer, VolunteerActivity, ActivityApplication, OrganizerActivity
 
 admin.site.register(Volunteer)
 admin.site.register(Organizer)
@@ -8,5 +8,6 @@ admin.site.register(Activity)
 admin.site.register(ActivityStatus)
 admin.site.register(VolunteerActivity)
 admin.site.register(ActivityApplication)
+admin.site.register(OrganizerActivity)
 
 # Register your models here.
